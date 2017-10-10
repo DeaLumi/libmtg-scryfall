d7c60b32ce23585fd11570b246c04ca2b2c995d2
Emi <dealuminis@gmail.com>
2017-10-10 11:27:14 -0700

    Add update and needsUpdate to Scryfall
    needsUpdate right now just reports whether its been a week yet.
    
    Signed-off-by: Emi <dealuminis@gmail.com>

--- src/main/java/emi/lib/mtg/scryfall/ScryfallDataSource.java
+++ src/main/java/emi/lib/mtg/scryfall/ScryfallDataSource.java
@@ -23,9 +23,12 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.time.Instant;
 import java.util.*;
+import java.util.function.DoubleConsumer;
 
 public class ScryfallDataSource implements DataSource {
 	private static final long UPDATE_INTERVAL = 7 * 24 * 60 * 60 * 1000;
+
+	private static final Path DATA_FILE = Paths.get("data", "scryfall", "data.json");
 
 	private static void expect(Object input, Object expected) throws IOException {
 		if (!Objects.equals(input, expected)) {
@@ -33,160 +36,21 @@ public class ScryfallDataSource implements DataSource {
 		}
 	}
 
-	private static int status = 0;
-
-	private static void advanceStatus() {
-		System.out.print("\033[1D");
-		switch (status % 4) {
-			case 0:
-				System.out.print("/");
-				break;
-			case 1:
-				System.out.print("-");
-				break;
-			case 2:
-				System.out.print("\\");
-				break;
-			case 3:
-				System.out.print("|");
-				break;
-			default:
-				System.out.print("?");
-				break;
-		}
-		System.out.flush();
-
-		++status;
-	}
-
 	private final BiMap<UUID, ScryfallPrinting> printings;
 	private final BiMap<String, ScryfallCard> cards;
 	private final BiMap<String, ScryfallSet> sets;
 
 	public ScryfallDataSource() throws IOException {
-		Path dataFile = Paths.get("data", "scryfall", "data.json");
-
-		if (!Files.exists(dataFile.getParent())) {
-			Files.createDirectories(dataFile.getParent());
-		}
-
-		if (!Files.exists(dataFile) || Instant.now().minusMillis(UPDATE_INTERVAL).isAfter(Files.getLastModifiedTime(dataFile).toInstant())) {
-			System.out.println("Sets file needs update. Please wait...");
-			Scryfall api = new Scryfall();
-			List<emi.lib.scryfall.api.Set> sets = api.sets();
-			List<emi.lib.scryfall.api.Card> cards = api.cards();
-			Set<String> droppedSets = new HashSet<>();
-
-			Path tmp = Files.createTempFile("scryfall-data", ".json");
-			JsonWriter writer = Scryfall.GSON.newJsonWriter(new OutputStreamWriter(Files.newOutputStream(tmp), StandardCharsets.UTF_8));
-
-			writer.beginObject();
-
-			writer.name("sets");
-			writer.beginObject();
-			for (emi.lib.scryfall.api.Set set : sets) {
-				System.out.print(" Set: " + set.code + " / " + set.name + "... ");
-
-				if (set.setType == SetType.Token) {
-					System.out.println("ignored (token set)");
-					droppedSets.add(set.code);
-					continue;
-				}
-
-				writer.name(set.code);
-				Scryfall.GSON.toJson(set, emi.lib.scryfall.api.Set.class, writer);
-				System.out.println("done!");
-			}
-			writer.endObject();
-
-			writer.name("printings");
-			writer.beginObject();
-			System.out.print("Cards  ");
-			System.out.flush();
-			int statusCounter = 0;
-			for (emi.lib.scryfall.api.Card card : cards) {
-				if (card.layout == CardLayout.Token || card.layout == CardLayout.DoubleFacedToken) {
-					continue;
-				}
-
-				if (droppedSets.contains(card.set)) {
-					continue;
-				}
-
-				// Null out some excess data here to save hard drive space.
-				card.legalities = null; // TODO: We might want to bring this back soon...
-
-				writer.name(card.id.toString());
-				Scryfall.GSON.toJson(card, emi.lib.scryfall.api.Card.class, writer);
-
-				++statusCounter;
-				if (statusCounter == 25) {
-					advanceStatus();
-					statusCounter = 0;
-				}
-			}
-			writer.endObject();
-
-			writer.endObject();
-			writer.close();
-
-			Files.copy(tmp, dataFile, StandardCopyOption.REPLACE_EXISTING);
-			Files.delete(tmp);
-
-			System.out.println();
-			System.out.println("Done! Cleaning up...");
-
-			System.gc();
-			System.gc();
-
-			System.out.println("Done! Next update in one week~");
-		}
-
-		BiMap<String, emi.lib.scryfall.api.Set> jsonSets = HashBiMap.create();
-		BiMap<UUID, emi.lib.scryfall.api.Card> jsonCards = HashBiMap.create();
-
-		JsonReader reader = Scryfall.GSON.newJsonReader(new InputStreamReader(Files.newInputStream(dataFile), StandardCharsets.UTF_8));
-
-		reader.beginObject();
-
-		expect(reader.nextName(), "sets");
-		reader.beginObject();
-		while (reader.peek() == JsonToken.NAME) {
-			String code = reader.nextName();
-			emi.lib.scryfall.api.Set set = Scryfall.GSON.fromJson(reader, emi.lib.scryfall.api.Set.class);
-			expect(code, set.code);
-
-			if (set.setType == SetType.Token) {
-				continue;
-			}
-
-			jsonSets.put(set.code, set);
-		}
-		reader.endObject();
-
-		expect(reader.nextName(), "printings");
-		reader.beginObject();
-		while (reader.peek() == JsonToken.NAME) {
-			String id = reader.nextName();
-			emi.lib.scryfall.api.Card card = Scryfall.GSON.fromJson(reader, emi.lib.scryfall.api.Card.class);
-			expect(id, card.id.toString());
-
-			if (card.layout == CardLayout.Token || card.layout == CardLayout.DoubleFacedToken) {
-				continue;
-			}
-
-			jsonCards.put(card.id, card);
-		}
-		reader.endObject();
-
 		this.cards = HashBiMap.create();
 		this.printings = HashBiMap.create();
 		this.sets = HashBiMap.create();
 
-		while (!jsonCards.isEmpty()) {
-			emi.lib.scryfall.api.Card jsonCard = jsonCards.values().iterator().next();
-
-			ScryfallCardFactory.create(jsonSets, jsonCards, jsonCard, sets, cards, printings);
+		try {
+			loadData();
+		} catch (IOException ioe) {
+			System.err.println("Scryfall data source couldn't load data file.");
+			ioe.printStackTrace();
+			System.err.println("We'll continue with no cards...");
 		}
 	}
 
@@ -220,11 +84,160 @@ public class ScryfallDataSource implements DataSource {
 		return sets.get(code);
 	}
 
+	@Override
+	public boolean update(DoubleConsumer progress) throws IOException {
+		if (!Files.exists(DATA_FILE.getParent())) {
+			Files.createDirectories(DATA_FILE.getParent());
+		}
+
+		System.out.println("Sets file needs update. Please wait...");
+		Scryfall api = new Scryfall();
+		List<emi.lib.scryfall.api.Set> sets = api.sets();
+		Set<String> droppedSets = new HashSet<>();
+
+		Path tmp = Files.createTempFile("scryfall-data", ".json");
+		JsonWriter writer = Scryfall.GSON.newJsonWriter(new OutputStreamWriter(Files.newOutputStream(tmp), StandardCharsets.UTF_8));
+
+		writer.beginObject();
+
+		writer.name("sets");
+		writer.beginObject();
+		for (emi.lib.scryfall.api.Set set : sets) {
+			System.out.print(" Set: " + set.code + " / " + set.name + "... ");
+
+			if (set.setType == SetType.Token) {
+				System.out.println("ignored (token set)");
+				droppedSets.add(set.code);
+				continue;
+			}
+
+			writer.name(set.code);
+			Scryfall.GSON.toJson(set, emi.lib.scryfall.api.Set.class, writer);
+			System.out.println("done!");
+		}
+		writer.endObject();
+
+		List<emi.lib.scryfall.api.Card> cards = api.cards();
+
+		writer.name("printings");
+		writer.beginObject();
+		System.out.print("Cards:     ");
+		System.out.flush();
+		int statusCounter = 0;
+		for (emi.lib.scryfall.api.Card card : cards) {
+			if (card.layout == CardLayout.Token || card.layout == CardLayout.DoubleFacedToken) {
+				continue;
+			}
+
+			if (droppedSets.contains(card.set)) {
+				continue;
+			}
+
+			// Null out some excess data here to save hard drive space.
+			card.legalities = null; // TODO: We might want to bring this back soon...
+
+			writer.name(card.id.toString());
+			Scryfall.GSON.toJson(card, emi.lib.scryfall.api.Card.class, writer);
+
+			++statusCounter;
+			double status = (double) statusCounter / (double) cards.size();
+			System.out.print(String.format("\033[4D% 3d%%", (int) (status * 100.0)));
+
+			if (progress != null) {
+				progress.accept(status);
+			}
+		}
+		writer.endObject();
+
+		writer.endObject();
+		writer.close();
+
+		Files.copy(tmp, DATA_FILE, StandardCopyOption.REPLACE_EXISTING);
+		Files.delete(tmp);
+
+		System.out.println();
+		System.out.println("Done! Cleaning up...");
+
+		System.gc();
+		System.gc();
+
+		System.out.println("Done! Next update in one week~");
+
+		loadData();
+
+		return true;
+	}
+
+	@Override
+	public boolean needsUpdate() {
+		try {
+			return !Files.exists(DATA_FILE) || Instant.now().minusMillis(UPDATE_INTERVAL).isAfter(Files.getLastModifiedTime(DATA_FILE).toInstant());
+		} catch (IOException ioe) {
+			System.err.println(String.format("Unable to check %s for freshness -- please update Scryfall data.", DATA_FILE.toString()));
+			ioe.printStackTrace();
+			return true;
+		}
+	}
+
+	private void loadData() throws IOException {
+		this.cards.clear();
+		this.printings.clear();
+		this.sets.clear();
+
+		BiMap<String, emi.lib.scryfall.api.Set> jsonSets = HashBiMap.create();
+		BiMap<UUID, emi.lib.scryfall.api.Card> jsonCards = HashBiMap.create();
+
+		JsonReader reader = Scryfall.GSON.newJsonReader(new InputStreamReader(Files.newInputStream(DATA_FILE), StandardCharsets.UTF_8));
+
+		reader.beginObject();
+
+		expect(reader.nextName(), "sets");
+		reader.beginObject();
+		while (reader.peek() == JsonToken.NAME) {
+			String code = reader.nextName();
+			emi.lib.scryfall.api.Set set = Scryfall.GSON.fromJson(reader, emi.lib.scryfall.api.Set.class);
+			expect(code, set.code);
+
+			if (set.setType == SetType.Token) {
+				continue;
+			}
+
+			jsonSets.put(set.code, set);
+		}
+		reader.endObject();
+
+		expect(reader.nextName(), "printings");
+		reader.beginObject();
+		while (reader.peek() == JsonToken.NAME) {
+			String id = reader.nextName();
+			emi.lib.scryfall.api.Card card = Scryfall.GSON.fromJson(reader, emi.lib.scryfall.api.Card.class);
+			expect(id, card.id.toString());
+
+			if (card.layout == CardLayout.Token || card.layout == CardLayout.DoubleFacedToken) {
+				continue;
+			}
+
+			jsonCards.put(card.id, card);
+		}
+		reader.endObject();
+
+		while (!jsonCards.isEmpty()) {
+			emi.lib.scryfall.api.Card jsonCard = jsonCards.values().iterator().next();
+
+			ScryfallCardFactory.create(jsonSets, jsonCards, jsonCard, sets, cards, printings);
+		}
+	}
+
 	public static void main(String[] args) throws IOException {
 		System.in.read();
 
 		long start = System.nanoTime();
 		ScryfallDataSource dataSource = new ScryfallDataSource();
+
+		if (dataSource.needsUpdate()) {
+			dataSource.update(null);
+		}
+
 		System.out.println(String.format("New: %.2f seconds", (System.nanoTime() - start) / 1e9));
 
 		System.out.println(String.format("New: %d sets, %d cards, %d printings", dataSource.sets.size(), dataSource.cards.size(), dataSource.printings.size()));
