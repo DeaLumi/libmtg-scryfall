72b9292d324a17c3cbe291ed9691102d7aa94aaa
Emi <dealuminis@gmail.com>
2021-11-13 23:23:34 -0800

    Add prototype Scryfall search provider
    Introduces a dependency on the deckbuilder itself, which is weird given that this is a plugin for that, but... since it's a plugin, it's not technically a dependency cycle. See?
    
    Anyway, this quick-and-dirty implementation forwards your request onto Scryfall and uses the returned cards to filter your search results. Unfortunately, since Scryfall paginates these requests and returns ALL card data, I had to limit the number of results to 10 pages, or 1750 printings.
    
    I've sent an e-mail to Scryfall about a more useful endpoint for this kind of application, but we'll have to wait on that. Meanwhile, this works for relatively precise searches.
    
    Signed-off-by: Emi <dealuminis@gmail.com>

---
+++ src/main/java/emi/lib/mtg/scryfall/ScryfallSearchProvider.java
@@ -0,0 +1,51 @@
+package emi.lib.mtg.scryfall;
+
+import emi.lib.mtg.scryfall.api.Card;
+import emi.lib.mtg.scryfall.api.PagedList;
+import emi.lib.mtg.scryfall.api.ScryfallApi;
+import emi.mtg.deckbuilder.model.CardInstance;
+
+import java.io.IOException;
+import java.util.Set;
+import java.util.UUID;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class ScryfallSearchProvider implements emi.mtg.deckbuilder.view.search.SearchProvider {
+	private final ScryfallApi api = new ScryfallApi();
+
+	@Override
+	public String name() {
+		return "Scryfall";
+	}
+
+	@Override
+	public String toString() {
+		return name();
+	}
+
+	@Override
+	public String usage() {
+		return String.join("\n",
+				"<p>Submits search requests to Scryfall and filters the results.",
+				"See <a href=\"https://scryfall.com/docs/syntax\">https://scryfall.com/docs/syntax</a> for a complete syntax guide.</p>",
+				"<p>Note that particularly large requests, such as <code>t:creature</code>, return too many results,",
+				"and due to a technical limitation on Scryfall's end, would cause the deckbuilder to spam Scryfall's servers to retrieve them all.",
+				"Any query returning more than 1750 results (10 pages) will produce a warning.</p>");
+	}
+
+	@Override
+	public Predicate<CardInstance> parse(String s) throws IllegalArgumentException {
+		try {
+			System.err.printf("Begin Scryfall search: %s", s);
+			PagedList<Card> cards = api.query(s);
+			System.err.printf(" ...%d results... ", cards.size());
+			if (cards.size() >= 175 * 10) throw new IllegalArgumentException(String.format("The search returned %d results. Please refine your search or use another search provider.", cards.size()));
+			Set<UUID> ids = cards.stream().map(c -> c.id).collect(Collectors.toSet());
+			System.err.printf("...fetched! Found %d IDs.\n", ids.size());
+			return ci -> ids.contains(ci.id()); // TODO: Wish I matched on set/collector number instead.
+		} catch (IOException ioe) {
+			throw new IllegalArgumentException("A network error occurred: " + ioe.getLocalizedMessage(), ioe);
+		}
+	}
+}
