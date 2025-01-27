6b0a8d39ecfcb56bba8d33582e3fe5826a584c66
Emi <dealuminis@gmail.com>
2025-01-27 08:41:23 -0800

    Add Update Tags plugin-prefs operation
    So users who change tags can easily and immediately update them.
    
    Signed-off-by: Emi <dealuminis@gmail.com>

--- src/main/java/emi/lib/mtg/scryfall/ScryfallPreferences.java
+++ src/main/java/emi/lib/mtg/scryfall/ScryfallPreferences.java
@@ -1,7 +1,12 @@
 package emi.lib.mtg.scryfall;
 
 import emi.lib.mtg.scryfall.serde.ScryfallSerde;
+import emi.mtg.deckbuilder.controller.Context;
+import emi.mtg.deckbuilder.controller.Tags;
+import emi.mtg.deckbuilder.controller.Updateable;
 import emi.mtg.deckbuilder.model.Preferences;
+
+import java.io.IOException;
 
 public class ScryfallPreferences implements Preferences.Plugin {
 	private static ScryfallPreferences DEFAULT_INSTANCE = null;
@@ -35,4 +40,20 @@ public class ScryfallPreferences implements Preferences.Plugin {
 
 	@Preference(value="Art Tags", tooltip="Comma-separated list of art tags which should be imported from Scryfall. CAUTION: Some tags match tens of thousands of total printings!")
 	public String artTags = "squirrel, turtle, lantern";
+
+	@Operation(value="Update Tags", text="Update", tooltip="Click to re-download the tagged cards from Scryfall. This takes a few minutes.", longRunning=true)
+	public void updateTags(Context context, Preferences prefs, Updateable.Progress progress) throws IOException {
+		ScryfallTagSource tags = Tags.PROVIDERS.stream()
+				.filter(p -> p instanceof ScryfallTagSource)
+				.map(p -> (ScryfallTagSource) p)
+				.findAny()
+				.orElse(null);
+
+		if (tags == null) {
+			throw new IllegalStateException("Scryfall tags are missing -- this shouldn't be possible!");
+		}
+
+		tags.update(prefs.dataPath, progress);
+		tags.load(context.data, prefs.dataPath, progress);
+	}
 }
